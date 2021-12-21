2b53bfc39516cfd1d22c52166fd0761e6396bcc0
Rohan Kansara <rohankansara1999@gmail.com>
2021-12-21 13:48:34 +0530

    Added tests for tdd

---
+++ src/test/java/com/hexad/library/scenario/BorrowBooksTest.java
@@ -0,0 +1,21 @@
+package com.hexad.library.scenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BorrowBooksTest {
+
+    @Test
+    void testBorrowBooksFromTheLibraryWhenMoreThanOneCopyExits() {
+
+    }
+
+    @Test
+    void testBorrowBooksFromTheLibraryWhenOnlyOneCopyExits() {
+
+    }
+}
