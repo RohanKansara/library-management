c00181d28429ae23fd70953f5e7d71ef08326d4f
Rohan Kansara <rohankansara1999@gmail.com>
2021-12-24 19:55:45 +0530

    Formatted code

--- src/main/java/com/hexad/library/controller/BookController.java
+++ src/main/java/com/hexad/library/controller/BookController.java
@@ -6,8 +6,6 @@ import com.hexad.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @CrossOrigin
 @RestController
 @RequestMapping("/book/")
